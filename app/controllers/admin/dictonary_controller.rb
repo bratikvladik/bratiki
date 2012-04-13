9e6489e7454f7ffa541a179ba556ec1358232c6f
bratiki <bratikvladik@gmail.com>
2012-04-13 14:41:08 -0700

    14.05.12

--- app/controllers/admin/dictonary_controller.rb
+++
@@ -1,6 +0,0 @@
-class Admin::DictonaryController < ApplicationController
-  before_filter :check_of_existence_administrator
-  before_filter :check_for_administrating
-  def index
-  end
-end
--- app/controllers/pages/admin_controller.rb
+++
@@ -1,6 +0,0 @@
-class Pages::AdminController < ApplicationController
-  before_filter :check_of_existence_administrator
-  before_filter :check_for_administrating
-  def index
-  end
-end
